9a013ba7d000f393f08d1a3db3dfb5808f6b9e3e
lfvperes <luisfvperes@gmail.com>
2025-07-22 19:10:45 -0300

    feat: :art: added query filters for the main fields of the City class, also added a filter by state code

--- bsky_cidades/filters.py
+++ bsky_cidades/filters.py
@@ -2,10 +2,15 @@ from django_filters import rest_framework as filters
 from .models import City
 
 class CityFilter(filters.FilterSet):
+    state_code = filters.CharFilter(method='filter_by_state_code', label='Filter by the first two digits of the city ID (State Code)')
+
     class Meta:
         model = City
-        # Define the fields from your model that you want to enable filtering on.
-        fields = {
-            'state': ['exact'], # Allow exact matches, e.g., state='TO'
-            'used': ['exact'],  # Allow exact matches, e.g., used=true
-        }
+        fields = ['state', 'used', 'id'] 
+
+    def filter_by_state_code(self, queryset, name, value):
+        """
+        Filters the queryset for cities where the 'id' field starts
+        with the provided two-digit state code.
+        """
+        return queryset.filter(id__startswith=value)
