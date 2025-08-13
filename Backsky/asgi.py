46eb508de6cdab2f27279b3f16723d24ff128077
lfvperes <luisfvperes@gmail.com>
2025-08-13 10:50:30 -0300

    refactor: :recycle: Change name to Backsky, update mentions and dependencies

--- Backsky/asgi.py
+++ Backsky/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CRUDsky project.
+ASGI config for Backsky project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRUDsky.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backsky.settings')
 
 application = get_asgi_application()
+
+
--- import_csv.py
+++ import_csv.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRUDsky.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backsky.settings')
 django.setup()
 
 import csv
